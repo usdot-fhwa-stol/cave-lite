6083dfb59fb92461e1a6ef42d95a28a8a6465073
jwillmartin <jw.martin@outlook.com>
2026-04-24 16:39:44 -0400

    modified:   scripts/install_dependencies.sh 	modified:   src/infrastructure/cave

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -29,15 +29,14 @@ pip install --upgrade pip
 
 # Manually install dependencies that fail on Raspberry Pi OS, else install using pip
 if [[ $distro = "Debian" ]]; then
-    wget https://files.pythonhosted.org/packages/f4/01/09aac6ea758ca7b7b1b4832c0c39003752ef7b6c1478e6db2f34171db3fe/pycrate-0.7.2.tar.gz
-    tar -xzf pycrate-0.7.2.tar.gz
-    cd pycrate-0.7.2
+    wget https://files.pythonhosted.org/packages/42/70/64a5b11e831dab532b4b61e684c5807a68daeb93b2bd4853975acaaf968e/pycrate-0.7.11.tar.gz
+    tar -xzf pycrate-0.7.11.tar.gz
+    cd pycrate-0.7.11
     python3 setup.py install
     cd ../
-    rm -rf pycrate-0.7.2 pycrate-0.7.2.tar.gz
+    rm -rf pycrate-0.7.11 pycrate-0.7.11.tar.gz
 else
-    sudo apt-get -y install python3-pip
-    pip3 install pycrate
+    pip3 install pycrate>=0.7.11
 fi
 
 # Install the j2735_202409 package (pulls pycrate as a transitive dependency)
